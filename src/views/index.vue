c87685c3e376c639b9b3712de07f8080c4d789a2
zhangqian <m18140539656@163.com>
2022-02-12 19:31:06 +0800

    初始化项目

---
+++ src/views/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+  components: {
+
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped lang="sass">
+
+</style>
